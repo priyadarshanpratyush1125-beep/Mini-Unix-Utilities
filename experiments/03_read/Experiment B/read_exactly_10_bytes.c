f1a3b37479a69411714a3251463f95966c8dff19
pratyush priyadarshan <priyadarshanpratyush1125@gmail.com>
2026-09-11 19:13:47 +0000

    adding exp B

---
+++ experiments/03_read/Experiment B/read_exactly_10_bytes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int fd;
+    char buffer[100];
+    ssize_t n;
+
+    fd = open("file.txt", O_RDONLY);
+
+    if (fd == -1)
+    {
+        perror("open");
+        return 1;
+    }
+
+    n = read(fd, buffer, 10);
+
+    if (n == -1)
+    {
+        perror("read");
+        close(fd);
+        return 1;
+    }
+
+    printf("read() returned: %zd\n", n);
+
+    write(STDOUT_FILENO, buffer, n);
+
+    close(fd);
+
+    return 0;
+}
