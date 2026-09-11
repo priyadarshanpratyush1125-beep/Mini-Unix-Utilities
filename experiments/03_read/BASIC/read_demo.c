366b8c192d131e5b8d056f60849d84a51a814c4c
pratyush priyadarshan <priyadarshanpratyush1125@gmail.com>
2026-09-11 19:12:30 +0000

    adding basic of read()

---
+++ experiments/03_read/BASIC/read_demo.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int fd;
+    char buffer[6];
+
+    fd = open("file.txt", O_RDONLY);
+
+    if (fd == -1)
+    {
+        perror("open");
+        return 1;
+    }
+
+    ssize_t n = read(fd, buffer, 5);
+
+    if (n == -1)
+    {
+        perror("read");
+        close(fd);
+        return 1;
+    }
+
+    printf("Bytes read = %zd\n", n);
+
+    printf("Data = "); //sinnce we store readed bytes in buffer[] which array so to traverse it we use for loop
+
+    for (ssize_t i = 0; i < n; i++)
+    {
+        printf("%c", buffer[i]);
+    }
+
+    printf("\n");
+
+    close(fd);
+
+    return 0;
+}
