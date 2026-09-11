7ee4f83e272a7e56a804c07f39ab079f827fb777
pratyush priyadarshan <priyadarshanpratyush1125@gmail.com>
2026-09-11 16:05:18 +0000

    adding comments in code

--- experiments/01_file_descriptor/fd_demo2.c
+++ experiments/01_file_descriptor/fd_demo2.c
@@ -1,8 +1,9 @@
-#include <unistd.h>
+#include <unistd.h>  // Required for the write() system call 
 
 int main(void)
 {
-    write(1, "Hello from stdout\n", 18);
+    write(1, "Hello from stdout\n", 18);  //synatx is ssize_t bytesWritten = write(int fd, const void *buf, size_t count);
+
 
     return 0;
 }
--- experiments/01_file_descriptor/fd_demo3.c
+++ experiments/01_file_descriptor/fd_demo3.c
@@ -1,8 +1,8 @@
-#include <unistd.h>
-
+#include <unistd.h> // Required for the write() system call 
 int main(void)
 {
-    write(1, "this is stdout\n", 15);
+    write(1, "this is stdout\n", 15);   //synatx is ssize_t bytesWritten = write(int fd, const void *buf, size_t count);
+
     write(2, "this is stderr\n", 15);
 
     return 0;
